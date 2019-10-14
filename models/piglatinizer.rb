e3734781970166d16979b9024d2f7d27983e404c
Karen Flores <karen.ag.flores@gmail.com>
2019-10-14 18:42:38 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,14 +5,19 @@ class PigLatinizer
     @text = text
   end
 
+  def vowel(char)
+    char.match(/[aeiouAEIOU]/)
+  end
+
   def piglatinize_word(word)
-    if word.scan(/[aeiouAEIOU]/)
+    if vowel(word[0])
       #leave the vowel and add w-ay at the end
       word = word + "w"
-    elsif
+    #elsif 
       #remove the first letter, add it to the end and add ay
-      word = word.slice(3..-1) + word.slice(0,3)
-    elsif #2
+      #word = word.slice(3..-1) + word.slice(0,3)
+    elsif vowel(word[0]) && vowel(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
     else
       word = word.slice(1..-1) + word.slice(0)
     end
