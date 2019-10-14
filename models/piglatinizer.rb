243fb53ce7944963c157c88313d6b5cf4292dc07
Karen Flores <karen.ag.flores@gmail.com>
2019-10-14 18:14:56 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,8 +1,16 @@
 class PigLatinizer
-  def initialize
+  attr_reader :text
+
+  def initialize(text)
+    @text = text
   end
 
   def piglatinize
+    if text.scan(/[aeiou]/)
+      #leave the vowel and add way at the end
+    else
+      #remove the first letter, add it to the end and add ay
+    end
   end
 
   def sentence
