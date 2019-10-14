78fc9499262f995ba519d1030028e94ff7e5c81b
Karen Flores <karen.ag.flores@gmail.com>
2019-10-14 18:18:48 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,14 +5,15 @@ class PigLatinizer
     @text = text
   end
 
-  def piglatinize
-    if text.scan(/[aeiou]/)
+  def piglatinize_word(word)
+    if word.scan(/[aeiou]/)
       #leave the vowel and add way at the end
+      
     else
       #remove the first letter, add it to the end and add ay
     end
   end
 
-  def sentence
+  def piglatinize_sentence(sentence)
   end
 end
